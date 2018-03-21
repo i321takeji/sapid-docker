d355490c8a95ca979555cf9e085aafb6f669e219
bambooG130 <i.321.takeji@gmail.com>
2018-03-21 12:34:31 +0900

    Added: sapid-ubuntu

---
+++ sapid-ubuntu/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export JAVA_HOME=`readlink -m "$(which javac)" | sed -e "s/\/bin\/javac$//g"`
+export PATH="$JAVA_HOME/bin:$PATH"
+
+if [ -f /usr/local/Sapid/lib/SetUp.sh ]; then
+    . /usr/local/Sapid/lib/SetUp.sh
+fi
+
+"$@"
