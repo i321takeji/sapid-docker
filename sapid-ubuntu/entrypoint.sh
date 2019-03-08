2e2ee3f26da52b662a96d297413d3d5f75fead24
bambooG130 <i.321.takeji@gmail.com>
2019-03-08 18:35:14 +0900

    Modify entripoint.sh

--- sapid-ubuntu/entrypoint.sh
+++ sapid-ubuntu/entrypoint.sh
@@ -9,13 +9,16 @@ if [ -z "${USERNAME}" ] || [ -z "${USERID}" ]; then
   . /usr/local/bin/sapid_env.sh && bash
 else
   useradd "${USERNAME}" -u "${USERID}" -m -d "/home/${USERNAME}" -s /bin/bash
+  echo "cd `pwd`" >> "/home/${USERNAME}/.profile"
 #  echo 'umask 022' >> "/home/${USERNAME}/.profile"
 #  echo '. /usr/local/bin/sapid_env.sh' >> "/home/${USERNAME}/.profile"
 
   shift; shift
-  CMD=""
-  if [ $# -gt 0 ]; then
+  CMD=''
+
+  if ! ( [ $# -eq 0 ] || ( [ $# -eq 1 ] && ( [ "$1" = 'sh' ] || [ "$1" = 'bash' ] ))); then
     CMD="-c '$@'"
   fi
+
   eval su - "${USERNAME}" "${CMD}"
 fi
--- sapid-ubuntu/runSapid.sh
+++ sapid-ubuntu/runSapid.sh
@@ -23,4 +23,5 @@ fi
 
 CMD="$@"
 
-bash -c "docker run -i -t --rm ${WORK_VOLUME} ${IMAGE} ${USER} ${UID} ${CMD}"
+#bash -c "docker run -i -t --rm ${WORK_VOLUME} ${IMAGE} ${USER} ${UID} ${CMD}"
+bash -c "docker run -i -t --rm ${WORK_VOLUME} ${IMAGE} sapider ${UID} ${CMD}"
